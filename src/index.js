d6875ece6aecf753ac05d4151d08a96cbf3ccb3c
hfdavis <149373126+hfdavis@users.noreply.github.com>
2024-02-02 11:18:26 +0000

    added basic javascript form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRiddle(event) {
+  event.preventDefault();
 
+  new Typewriter("#riddle", {
+    strings: "I speak without a mouth and hear without ears.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let riddleFormElement = document.querySelector("#riddle-generator-form");
+riddleFormElement.addEventListener(`submit`, generateRiddle);
