aac6280bad37b191355e03bafdf80a4984766f09
hfdavis <149373126+hfdavis@users.noreply.github.com>
2024-02-03 17:02:14 +0000

    improved user experience

--- src/index.js
+++ src/index.js
@@ -12,12 +12,13 @@ function generateRiddle(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "4cfe24t3503o158b86cbac58e0e08cf3";
-  let prompt = `generate a short riddle that is ${instructionsInput.value} difficulty and include answer.`;
+  let prompt = `generate a short riddle that is ${instructionsInput.value} in its difficulty to solve and include answer.`;
   let context =
-    "you are a clever riddle expert and love to write short riddles. your mission is to generate a riddle in basic HTML, the riddle should never be about HTML. display the riddle and answer only. make sure to follow the user instructions";
+    "you are a clever riddle expert and love to write short riddles. your mission is to generate a riddle in basic HTML, the riddle should never be about HTML. display the riddle and answer only. make sure to follow the user instructions the riddle should be easy, medium or hard in difficulty depending on what option the user selects";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log(`prompt`);
+  let riddleElement = document.querySelector("#riddle");
+  riddleElement.classList.remove("hidden");
 
   axios.get(apiUrl).then(displayRiddle);
 }
